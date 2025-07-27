e8175a5c60e13892a8ce763221bfcddf73dd4e63
Argon0ut <nusupoversultan228@gmail.com>
2025-07-28 00:26:21 +0500

    Ticket Endpoints and DB display fixing

---
+++ backend/app/schemas/ticket.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+from backend.app.models.ticket import TicketStatus
+
+from backend.app.models.ticket import Ticket
+from backend.app.models.user import User
+
+
+class UserSchema(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+class CreateTicket(BaseModel):
+    user: UserSchema
+    title: str
+    description: str
+    status: str
+    created_at: datetime
+
+
+    class Config:
+        orm_mode = True
+
+
+class ReadTicket(BaseModel):
+    id: int
+    user: UserSchema
+    title: str
+
+class UpdateTicketStatus(BaseModel):
+    status: TicketStatus
