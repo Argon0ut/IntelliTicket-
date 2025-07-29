7b83b6ea0e87aa4cd6e4e7899eb95c6032dc500a
Argon0ut <nusupoversultan228@gmail.com>
2025-07-29 19:51:24 +0500

    setting vue

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from starlette import status
 from backend.app.api.routes import users, auth, tickets
 from backend.app.core.dependencies import db_dependency, user_dependency
 from backend.app.database.session import create_tables
-
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,6 +16,14 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(tickets.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
